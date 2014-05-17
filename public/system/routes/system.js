0906790dea1eb540bee7e3f43a8ac40401aa663f
RayTaylorLin <raytaylorlin@gmail.com>
2014-05-17 22:13:01 +0800

    新增通讯录的客户端模块（待修正），添加基本的菜单栏

--- public/system/routes/system.js
+++ public/system/routes/system.js
@@ -1,24 +1,36 @@
 'use strict';
 
 //Setting up route
-angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
+angular.module('mean.system')
+    .config(['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
             // For unmatched routes:
             $urlRouterProvider.otherwise('/');
 
             // states for my app
-            $stateProvider              
+            $stateProvider
+                .state('duty', {
+                    url: '/duty',
+                    templateUrl: 'public/duty/views/index.html'
+                })
+                .state('contacts', {
+                    url: '/contacts',
+                    templateUrl: 'public/contacts/views/index.html'
+                })
                 .state('home', {
                     url: '/',
                     templateUrl: 'public/system/views/index.html'
-                })
-                .state('auth', {
-                    templateUrl: 'public/auth/views/index.html'
                 });
         }
     ])
     .config(['$locationProvider',
         function($locationProvider) {
-            $locationProvider.hashPrefix('!');
+            // 若浏览器支持HTML5的history API，则删除URL的#
+            if(window.history && window.history.pushState){
+                $locationProvider.html5Mode(true);
+            } else {
+                // 否则只能强制浏览器使用带#的URL
+                $locationProvider.hashPrefix('!');                
+            }
         }
     ]);
