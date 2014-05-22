868c7b0d38228cf638e459ce67452ecb83067708
RayTaylorLin <raytaylorlin@gmail.com>
2014-05-22 20:14:35 +0800

    重命名contacts控制器

---
+++ public/contacts/controllers/contacts_controllers.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('elwglife.contacts.controllers', [])
+    .controller('ContentController', ['$scope', '$rootScope',
+        function($scope, $rootScope) {
+
+        }
+    ]);
