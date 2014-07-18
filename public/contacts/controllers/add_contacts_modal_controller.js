858370c30e5e4d8da820c5998cc0eff72cf1cce2
RayTaylorLin <raytaylorlin@gmail.com>
2014-07-18 20:15:38 +0800

    修改通讯录弹出框样式

--- public/contacts/controllers/add_contacts_modal_controller.js
+++ public/contacts/controllers/add_contacts_modal_controller.js
@@ -4,12 +4,21 @@ var contactsModule = angular.module('elwglife.contacts');
 
 contactsModule.controller('AddContactsModalController', 
     function($scope, $modalInstance) {
-        $scope.ok = function() {
+        $scope.gradeList = ['14级', '13级', '12级', '老师'];
+        $scope.teacherList = ['许炜', '程文青', '夏天', '黑晓军'];
+
+        $scope.contacts = {
+            sex: 'male',
+            grade: $scope.gradeList[0],
+            teacher: $scope.teacherList[0]
+        };
+
+        $scope.submit = function() {
             console.log('ok');
         };
 
         $scope.cancel = function() {
-            console.log('cancel');
+            $modalInstance.dismiss('cancel');
         };
     }
 );
