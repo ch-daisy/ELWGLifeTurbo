ba67b6d7b72d8ce57a20b13326795b169957a749
RayTaylorLin <raytaylorlin@gmail.com>
2014-07-25 16:13:25 +0800

    完善检查通讯录是否存在功能，若存在则显示删除按钮

--- public/contacts/controllers/add_contacts_modal_controller.js
+++ public/contacts/controllers/add_contacts_modal_controller.js
@@ -4,10 +4,11 @@ var contactsModule = angular.module('elwglife.contacts');
 
 contactsModule.controller('AddContactsModalCtrl', 
     function($scope, $rootScope, $modalInstance, $location, Contacts) {
+        var defaultContacts;
+
         $scope.gradeList = ['14级学硕', '14级工硕', '13级学硕', '13级工硕', '12级学硕', '12级工硕', '本科实习生', '老师'];
         $scope.teacherList = ['许炜', '程文青', '夏天', '黑晓军'];
-
-        $scope.contacts = new Contacts({
+        $scope.contacts = defaultContacts = new Contacts({
             sex: '男',
             grade: $scope.gradeList[0],
             teacher: $scope.teacherList[0]
@@ -16,11 +17,25 @@ contactsModule.controller('AddContactsModalCtrl',
         // 通过姓名检查通讯录是否已存在
         $scope.checkExist = function(name) {
             Contacts.query(function(contactsList) {
+                var contactsExist = false,
+                    originName, contacts;
+
+                // 遍历现有通讯录看名字是否已存在
                 angular.forEach(contactsList, function(contacts) {
                     if(contacts.name === name) {
                         $scope.contacts = contacts;
+                        contactsExist = true;
                     }
                 });
+
+                if(!contactsExist) {
+                    originName = $scope.contacts.name;
+                    $scope.contacts = defaultContacts;
+                    $scope.contacts.name = originName;
+                }
+
+                // 该标记主要控制删除按钮是否出现，以及控制提交的HTTP方法
+                $scope.contactsExist = contactsExist;
             });
         };
 
