7138baebb3927e56b97686ed625c91d56cb3fb19
RayTaylorLin <raytaylorlin@gmail.com>
2014-08-17 10:46:14 +0800

    更新Footer的作者信息

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -2,6 +2,9 @@
 
 var Contacts = require('../models/contacts');
 
+/**
+ * 获取通讯录
+ */
 exports.getContacts = function(req, res) {
     Contacts.find(function (err, data) {
         if (err) {
@@ -55,8 +58,8 @@ exports.updateContacts = function(req, res) {
 };
 
 /**
-  * 删除通讯录记录
-  */
+ * 删除通讯录记录
+ */
 exports.deleteContacts = function(req, res) {
     Contacts.remove({'_id':req.params.id}, function (err) {
         res.json({
@@ -64,67 +67,3 @@ exports.deleteContacts = function(req, res) {
         });
     });
 };
-
-/**
- * Create user
- */
-// exports.create = function(req, res, next) {
-//     var user = new User(req.body);
-
-//     user.provider = 'local';
-
-//     // because we set our user.provider to local our models/user.js validation will always be true
-//     req.assert('email', 'You must enter a valid email address').isEmail();
-//     req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
-//     req.assert('username', 'Username cannot be more than 20 characters').len(1,20);
-//     req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-
-//     var errors = req.validationErrors();
-//     if (errors) {
-//         return res.status(400).send(errors);
-//     }
-
-//     // Hard coded for now. Will address this with the user permissions system in v0.3.5
-//     user.roles = ['authenticated'];
-//     user.save(function(err) {
-//         if (err) {
-//             switch (err.code) {
-//                 case 11000:
-//                 case 11001:
-//                     res.status(400).send('Username already taken');
-//                     break;
-//                 default:
-//                     res.status(400).send('Please fill all the required fields');
-//             }
-
-//             return res.status(400);
-//         }
-//         req.logIn(user, function(err) {
-//             if (err) return next(err);
-//             return res.redirect('/');
-//         });
-//         res.status(200);
-//     });
-// };
-// /**
-//  * Send User
-//  */
-// exports.me = function(req, res) {
-//     res.jsonp(req.user || null);
-// };
-
-// /**
-//  * Find user by id
-//  */
-// exports.user = function(req, res, next, id) {
-//     User
-//         .findOne({
-//             _id: id
-//         })
-//         .exec(function(err, user) {
-//             if (err) return next(err);
-//             if (!user) return next(new Error('Failed to load User ' + id));
-//             req.profile = user;
-//             next();
-//         });
-// };
