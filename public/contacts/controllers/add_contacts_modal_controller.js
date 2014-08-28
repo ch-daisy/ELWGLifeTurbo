2734b3d27c9f2c4c03fe1fe6a39dc8d51b3c1a14
RayTaylorLin <raytaylorlin@gmail.com>
2014-08-28 14:40:49 +0800

    修复通讯录QQ一栏数字限制的问题，更新老师和年级选项

--- public/contacts/controllers/add_contacts_modal_controller.js
+++ public/contacts/controllers/add_contacts_modal_controller.js
@@ -11,8 +11,8 @@ contactsModule.controller('AddContactsModalCtrl',
             $rootScope.$broadcast('refreshContacts');
         }
 
-        $scope.gradeList = ['14级学硕', '14级工硕', '13级学硕', '13级工硕', '12级学硕', '12级工硕', '本科实习生', '老师'];
-        $scope.teacherList = ['许炜', '程文青', '夏天', '黑晓军'];
+        $scope.gradeList = ['14级学硕', '14级工硕', '13级学硕', '13级工硕', '12级学硕', '12级工硕', '本科实习生', '博士生', '教师'];
+        $scope.teacherList = ['许炜', '程文青', '夏天', '黑晓军', '刘威', '徐晶'];
         $scope.contacts = defaultContacts = new Contacts({
             sex: '男',
             grade: $scope.gradeList[0],
